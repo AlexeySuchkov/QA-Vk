09f0701b07a7723a87193f89f8ecc8ae9141f54c
Alexey Suchkov <alexey.suchkov@gmail.com>
2020-04-23 01:53:30 +0300

    v1

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private int count;
+    private boolean registered;
+    private boolean canPost;
+    private String text;
+
+    // + get/set на все поля
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Post {
+    private int postId;
+    private int posterId;
+    private String posterName;
+    private String postDate;
+    private String text;
+    private String imageUrl;
+    private int likesCounter;
+    private int viewsCounter;
+    private Repost repost;
+    private CommentsInfo commentsInfo;
+
+    // + get/set на все поля
+}
