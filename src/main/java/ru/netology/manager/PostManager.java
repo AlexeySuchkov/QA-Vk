c81f155942c37b6b9a307bb8c3348717576df629
Alexey Suchkov <alexey.suchkov@gmail.com>
2020-04-23 17:35:37 +0300

    Post manager added

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,20 @@
+package ru.netology.manager;
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search() {
+
+        // TODO: add logic
+        return null;
+    }
+
+    public Post[] delete() {
+
+        // TODO: add logic
+        return null;
+    }
+
+
+}
